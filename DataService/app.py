aee5310f08017721f36872df7a8f2ce5936515c5
Dr. Clocktor <jonas@sakejo.dk>
2025-11-03 16:35:48 +0100

    Database oprettelse og API

--- DataService/app.py
+++ DataService/app.py
@@ -1,14 +1,25 @@
+from flask import Flask, jsonify
 import sqlite3
 import pandas as pd
 
-df = pd.read_excel("data.xlsx")
+
+
+
+
+
+
+
+
+
+
+
+'''df = pd.read_excel("data.xlsx")
 
 conn = sqlite3.connect("data.db")
 df.to_sql("records", conn, if_exists="replace", index=False)
-
-
-##unktion der printer hele databasen bare for at teste at det virker
-def printData():
+'''
+##Funktion der printer hele databasen bare for at teste at det virker
+'''def printData():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM records")
     rows = cursor.fetchall()
@@ -16,4 +27,4 @@ def printData():
         print(row)
 
 #printData() #der er 1000 rækker så ikke print den hele for sjov bby
-
+'''
